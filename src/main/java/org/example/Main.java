6cb9ca0b7d8c1c94e4f641c38e17b6feb1405ad3
richardvn <richardpadilla3rod@gmail.com>
2024-05-29 21:19:36 -0500

    El main fue modificado por ambos colaboradores

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,32 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        //esta vez pruebo yo
+        //creación de objetos
+        Hoteles hotel1 = new Hoteles(40,5,4.3,63.4,"Colon");
+        Hoteles hotel2 = new Hoteles(35,4,4.1,62.5,"Maverick");
+        Hoteles hotel3 = new Hoteles(25,3,3.9,58.4,"San Antonio");
+        Hoteles hotel4 = new Hoteles(43,7,4.5,70.0,"Marriot");
+        Hoteles hotel5 = new Hoteles(23,3,3.6,40.0,"Quito");
+        Hoteles hotel6 = new Hoteles();
+        Hoteles hotel7 = new Hoteles();
+
+        hotel3.setNombre(null);
+        hotel3.setCalificacion(0);
+        hotel3.setHabitaciones(0);    //seteo de valores a null o 0
+        hotel3.setPisos(0);
+        hotel3.setArea(0);
+
+        hotel6.modificar();
+        hotel7.modificar();
+        hotel3.modificar();  //llamando a la función modificar para cambiar los valores de los objetos
+
+        hotel1.mostrar();
+        hotel2.mostrar();
+        hotel3.mostrar();
+        hotel4.mostrar();    // impresión de los atributos de todos los objetos con la función mostrar
+        hotel5.mostrar();
+        hotel6.mostrar();
+        hotel7.mostrar();
     }
 }

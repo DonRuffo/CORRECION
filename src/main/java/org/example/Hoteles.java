45cbf7eeaf73790c51816ef3e8806ed06e38f20e
donruffo-admin <dennisdiaz407@gmail.com>
2024-05-29 20:47:48 -0500

    paso1

--- src/main/java/org/example/Hoteles.java
+++ src/main/java/org/example/Hoteles.java
@@ -1,8 +1,17 @@
 package org.example;
 
+import java.util.Scanner;
 public class Hoteles {
     int habitaciones, pisos;
     double calificacion, area;
     String nombre;
 
+    public Hoteles(){}
+    public Hoteles(int habitaciones, int pisos, double calificacion, double area, String nombre){
+        this.habitaciones = habitaciones;
+        this.pisos = pisos;
+        this.calificacion = calificacion;
+        this.area = area;
+        this.nombre = nombre;
+    }
 }

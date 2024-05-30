330afe79b529e91c8ef03cc59233d9961c5fc2b6
richardvn <richardpadilla3rod@gmail.com>
2024-05-29 21:19:36 -0500

    Funcion mostrar y modificar fue codificado por ambos colaboradores.

--- src/main/java/org/example/Hoteles.java
+++ src/main/java/org/example/Hoteles.java
@@ -60,4 +60,27 @@ public class Hoteles {
         return nombre;
     }
 
+    //Richard Padilla
+    public void mostrar(){
+        System.out.println("Nombre: "+this.getNombre());
+        System.out.println("Habitaciones: "+this.getHabitaciones());
+        System.out.println("Pisos: "+this.getPisos());
+        System.out.println("Calificacion: "+this.getCalificacion());
+        System.out.println("Area: "+this.getArea());
+    }
+
+    //Dennis Diaz
+    public void modificar(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el nombre: ");
+        String nombre = sc.nextLine(); this.setNombre(nombre);
+        System.out.println("Ingrese el habitacion: ");
+        int habitaciones = sc.nextInt(); this.setHabitaciones(habitaciones);
+        System.out.println("Ingrese el piso: ");
+        int pisos = sc.nextInt(); this.setPisos(pisos);
+        System.out.println("Ingrese el calificacion: ");
+        double calificacion = sc.nextDouble(); this.setCalificacion(calificacion);
+        System.out.println("Ingrese el area: ");
+        double area = sc.nextDouble(); this.setArea(area);
+    }
 }
